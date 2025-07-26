2eb7c45f3eee084bbe19b7a2d5f9202403fc7e26
Axel <nigthmaresans2@gmail.com>
2025-07-25 20:46:24 -0600

    fix: enhance error handling in Application with improved thread exception management

--- src/main.py
+++ src/main.py
@@ -1,11 +1,13 @@
 import gi
 import sys
+import threading
 from rich.traceback import install
 
 install(show_locals=True)
-
 gi.require_versions({"Adw": "1", "Gtk": "4.0"})
 from gi.repository import Gtk, Adw, Gio, Gdk
+
+from .modules.utils import idle
 from .ui.window import BananaWindow
 
 
@@ -16,20 +18,29 @@ class Application(Adw.Application):
             flags=Gio.ApplicationFlags.DEFAULT_FLAGS,
         )
         self.win = None
+        self.old_thread_hook = threading.excepthook
         self.old_hook = sys.excepthook
+
         sys.excepthook = self.exception_hook
+        threading.excepthook = self.thread_hook
+
+    def thread_hook(self, args: threading.ExceptHookArgs):
+        self.old_thread_hook(args)
+        self.present_err_diag(args.exc_type.__name__, " ".join(args.exc_value.args))
 
     def exception_hook(self, exc_type, exc_value, exc_traceback):
         self.old_hook(exc_type, exc_value, exc_traceback)
+        self.present_err_diag(exc_type.__name__, " ".join(exc_value.args))
 
+    def present_err_diag(self, title, body):
         diag = Adw.AlertDialog.new(
-            exc_type.__name__,
-            f"{' '.join(exc_value.args)}. Report this error to the github repository",
+            title,
+            f"{body}.\nReport this error to the github repository",
         )
 
         diag.add_response("a", "Accept")
 
-        diag.present(self.win)
+        idle(diag.present, self.win)
 
     def do_activate(self):
         display = Gdk.Display.get_default()
